d6d46cd9b3393e343075d916db8ad093eb33f0ad
countd360 <39341532+countd360@users.noreply.github.com>
2018-05-17 12:14:12 +0900

    Import
    Import from SVN

---
+++ ArrayConvertor/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace ArrayConvertor
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCArray = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBinary = new System.Windows.Forms.TextBox();
+            this.txtBinLineNo = new System.Windows.Forms.TextBox();
+            this.txtBinLineOffset = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numDataNumberInLine = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnToBinary = new System.Windows.Forms.Button();
+            this.btnToCArray = new System.Windows.Forms.Button();
+            this.btnSaveBinary = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numDataNumberInLine)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtCArray
+            // 
+            this.txtCArray.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtCArray.Location = new System.Drawing.Point(12, 55);
+            this.txtCArray.Multiline = true;
+            this.txtCArray.Name = "txtCArray";
+            this.txtCArray.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtCArray.Size = new System.Drawing.Size(432, 550);
+            this.txtCArray.TabIndex = 3;
+            this.txtCArray.WordWrap = false;
+            this.txtCArray.TextChanged += new System.EventHandler(this.txtCArray_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "C配列";
+            // 
+            // txtBinary
+            // 
+            this.txtBinary.AllowDrop = true;
+            this.txtBinary.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtBinary.Location = new System.Drawing.Point(622, 55);
+            this.txtBinary.Multiline = true;
+            this.txtBinary.Name = "txtBinary";
+            this.txtBinary.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtBinary.Size = new System.Drawing.Size(418, 550);
+            this.txtBinary.TabIndex = 6;
+            this.txtBinary.WordWrap = false;
+            this.txtBinary.TextChanged += new System.EventHandler(this.txtBinary_TextChanged);
+            this.txtBinary.DragDrop += new System.Windows.Forms.DragEventHandler(this.txtBinary_DragDrop);
+            this.txtBinary.DragEnter += new System.Windows.Forms.DragEventHandler(this.txtBinary_DragEnter);
+            // 
+            // txtBinLineNo
+            // 
+            this.txtBinLineNo.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtBinLineNo.Location = new System.Drawing.Point(535, 55);
+            this.txtBinLineNo.Multiline = true;
+            this.txtBinLineNo.Name = "txtBinLineNo";
+            this.txtBinLineNo.ReadOnly = true;
+            this.txtBinLineNo.Size = new System.Drawing.Size(81, 550);
+            this.txtBinLineNo.TabIndex = 0;
+            this.txtBinLineNo.TabStop = false;
+            this.txtBinLineNo.Text = "00";
+            this.txtBinLineNo.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtBinLineOffset
+            // 
+            this.txtBinLineOffset.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtBinLineOffset.Location = new System.Drawing.Point(622, 27);
+            this.txtBinLineOffset.Name = "txtBinLineOffset";
+            this.txtBinLineOffset.ReadOnly = true;
+            this.txtBinLineOffset.Size = new System.Drawing.Size(396, 22);
+            this.txtBinLineOffset.TabIndex = 2;
+            this.txtBinLineOffset.TabStop = false;
+            this.txtBinLineOffset.Text = "00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(532, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "バイナリ";
+            // 
+            // numDataNumberInLine
+            // 
+            this.numDataNumberInLine.Location = new System.Drawing.Point(383, 28);
+            this.numDataNumberInLine.Maximum = new decimal(new int[] {
+            16,
+            0,
+            0,
+            0});
+            this.numDataNumberInLine.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numDataNumberInLine.Name = "numDataNumberInLine";
+            this.numDataNumberInLine.Size = new System.Drawing.Size(61, 22);
+            this.numDataNumberInLine.TabIndex = 4;
+            this.numDataNumberInLine.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.numDataNumberInLine.ValueChanged += new System.EventHandler(this.numDataNumberInLine_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(235, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 15);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "一行当たりのデータ数：";
+            // 
+            // btnToBinary
+            // 
+            this.btnToBinary.Location = new System.Drawing.Point(450, 55);
+            this.btnToBinary.Name = "btnToBinary";
+            this.btnToBinary.Size = new System.Drawing.Size(79, 56);
+            this.btnToBinary.TabIndex = 0;
+            this.btnToBinary.Text = "＞＞＞";
+            this.btnToBinary.UseVisualStyleBackColor = true;
+            this.btnToBinary.Click += new System.EventHandler(this.btnToBinary_Click);
+            // 
+            // btnToCArray
+            // 
+            this.btnToCArray.Location = new System.Drawing.Point(450, 143);
+            this.btnToCArray.Name = "btnToCArray";
+            this.btnToCArray.Size = new System.Drawing.Size(79, 56);
+            this.btnToCArray.TabIndex = 1;
+            this.btnToCArray.Text = "＜＜＜";
+            this.btnToCArray.UseVisualStyleBackColor = true;
+            this.btnToCArray.Click += new System.EventHandler(this.btnToCArray_Click);
+            // 
+            // btnSaveBinary
+            // 
+            this.btnSaveBinary.Location = new System.Drawing.Point(450, 238);
+            this.btnSaveBinary.Name = "btnSaveBinary";
+            this.btnSaveBinary.Size = new System.Drawing.Size(79, 56);
+            this.btnSaveBinary.TabIndex = 2;
+            this.btnSaveBinary.Text = "ファイル保存";
+            this.btnSaveBinary.UseVisualStyleBackColor = true;
+            this.btnSaveBinary.Click += new System.EventHandler(this.btnSaveBinary_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1052, 617);
+            this.Controls.Add(this.btnSaveBinary);
+            this.Controls.Add(this.btnToCArray);
+            this.Controls.Add(this.btnToBinary);
+            this.Controls.Add(this.numDataNumberInLine);
+            this.Controls.Add(this.txtBinLineOffset);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBinLineNo);
+            this.Controls.Add(this.txtBinary);
+            this.Controls.Add(this.txtCArray);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "配列←→バイナリ変換ツール";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numDataNumberInLine)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCArray;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBinary;
+        private System.Windows.Forms.TextBox txtBinLineNo;
+        private System.Windows.Forms.TextBox txtBinLineOffset;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numDataNumberInLine;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnToBinary;
+        private System.Windows.Forms.Button btnToCArray;
+        private System.Windows.Forms.Button btnSaveBinary;
+    }
+}
+
---
+++ ArrayConvertor/Form1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ArrayConvertor
+{
+    public partial class frmMain : Form
+    {
+        enum LineFormat
+        {
+            WithHexPrefix = 0x0001,
+            WithoutHexPrefix = 0x0002,
+
+            SepratedBySpace = 0x0010,
+            SepratedByComma = 0x0020,
+
+        }
+
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        #region EventHandler
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+        private void txtBinary_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Link;
+        }
+
+        private void txtBinary_DragDrop(object sender, DragEventArgs e)
+        {
+            // Handle FileDrop data.
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                // Assign the file names to a string array, in 
+                // case the user has selected multiple files.
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                try
+                {
+                    if (files.Length > 1)
+                    {
+                        MessageBox.Show("複数のファイルを同時に開けません!一つにしてください。");
+                        return;
+                    }
+
+                    loadBinaryFile(files[0]);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+            }
+        }
+
+        private void txtBinary_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCArray_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numDataNumberInLine_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnToBinary_Click(object sender, EventArgs e)
+        {
+            // get data
+            byte[] data = dataFormatConvert(txtCArray.Lines);
+            if (data == null || data.Length == 0) return;
+
+            // convert
+            int numInLine = 16;
+            List<string> outputBuffer = new List<string>();
+            for (int i = 0; i < data.Length; i += numInLine)
+            {
+                int remainByte = data.Length - i < numInLine ? data.Length - i : numInLine;
+                byte[] lineData = new byte[remainByte];
+                Buffer.BlockCopy(data, i, lineData, 0, remainByte);
+
+                string line = lineFormatConvert(lineData, LineFormat.WithoutHexPrefix | LineFormat.SepratedBySpace);
+
+                outputBuffer.Add(line);
+            }
+
+            // set 
+            txtBinary.Lines = outputBuffer.ToArray();
+        }
+
+        private void btnToCArray_Click(object sender, EventArgs e)
+        {
+            // get data
+            byte[] data = dataFormatConvert(txtBinary.Lines);
+            if (data == null || data.Length == 0) return;
+
+            // convert
+            int numInLine = (int)numDataNumberInLine.Value;
+            List<string> outputBuffer = new List<string>();
+            for (int i = 0; i < data.Length; i += numInLine)
+            {
+                int remainByte = data.Length - i < numInLine ? data.Length - i : numInLine;
+                byte[] lineData = new byte[remainByte];
+                Buffer.BlockCopy(data, i, lineData, 0, remainByte);
+
+                string line = lineFormatConvert(lineData, LineFormat.WithHexPrefix | LineFormat.SepratedByComma);
+                if (i + remainByte >= data.Length)
+                {
+                    // last line
+                    line = line.TrimEnd(new char[] { ',' });
+                }
+
+                outputBuffer.Add(line);
+            }
+
+            // set
+            txtCArray.Lines = outputBuffer.ToArray();
+        }
+
+        private void btnSaveBinary_Click(object sender, EventArgs e)
+        {
+            // get data
+            byte[] data = dataFormatConvert(txtBinary.Lines);
+            if (data == null || data.Length == 0) return;
+
+            // get filename
+            SaveFileDialog sfd = new SaveFileDialog();
+            
+            if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string filename = sfd.FileName;
+
+                using (BinaryWriter binWriter = new BinaryWriter(File.Open(filename, FileMode.Create)))
+                {
+                    binWriter.Write(data);
+
+                    binWriter.Close();
+                }
+            }
+        }
+        #endregion
+
+        #region PrivateFunction
+        private void loadBinaryFile(string filename)
+        {
+            FileInfo fi = new FileInfo(filename);
+            long fileSize = fi.Length;
+            Console.WriteLine("Load binary file {0}, {1}bytes", filename, fileSize);
+
+            int fileSizeLength = 1;
+            while (fileSize > 16)
+            {
+                fileSize /= 16;
+                fileSizeLength++;
+            }
+            if (fileSizeLength < 2) fileSizeLength = 2;
+            string lineOffsetFormat = "x" + fileSizeLength.ToString();
+
+            using (BinaryReader binReader = new BinaryReader(File.Open(filename, FileMode.Open)))
+            {
+                long lineOffset = 0;
+                List<string> outputLines = new List<string>();
+                List<string> outputLineNo = new List<string>();
+                    
+                do
+                {
+                    byte[] lineData = binReader.ReadBytes(16);
+                    if (lineData.Length == 0) break;
+
+                    string dispayLine = lineFormatConvert(lineData, LineFormat.WithoutHexPrefix | LineFormat.SepratedBySpace);
+                    outputLines.Add(dispayLine);
+
+                    string lineNo = lineOffset.ToString(lineOffsetFormat);
+                    outputLineNo.Add(lineNo);
+
+                    lineOffset += 16;
+
+                } while (true);
+
+                txtBinary.Lines = outputLines.ToArray();
+                txtBinLineNo.Lines = outputLineNo.ToArray();
+
+                binReader.Close();
+            }
+        }
+
+        private string lineFormatConvert(byte[] data, LineFormat outputFormat)
+        {
+            string formatter = "";
+            // prefix
+            if ((outputFormat & LineFormat.WithHexPrefix) == LineFormat.WithHexPrefix)
+            {
+                formatter += "0x{0,2:x2}";
+            }
+            else
+            {
+                formatter += "{0,2:x2}";
+            }
+            // seprator
+            if ((outputFormat & LineFormat.SepratedByComma) == LineFormat.SepratedByComma)
+            {
+                formatter += ", ";
+            }
+            else
+            {
+                formatter += " ";
+            }
+
+            // convert line
+            string outString = "";
+            foreach (byte ch in data)
+            {
+                outString += String.Format(formatter, ch);
+            }
+            return outString.Trim();
+        }
+
+        private byte[] dataFormatConvert(string[] lines)
+        {
+            try
+            {
+                List<byte> outputBuffer = new List<byte>();
+                foreach (string line in lines)
+                {
+                    string[] byteDatas = line.Split(new Char[] { ' ', ',', '.', ':', '\t' });
+
+                    foreach (string byteData in byteDatas)
+                    {
+                        if (byteData.Length == 0) continue;
+
+                        string numberString = byteData;
+                        if (numberString.Length > 2 && numberString[0] == '0' && numberString[1] == 'x')
+                        {
+                            numberString = numberString.Substring(2);
+                        }
+
+                        byte data = byte.Parse(numberString, System.Globalization.NumberStyles.AllowHexSpecifier);
+                        outputBuffer.Add(data);
+                    }
+                }
+
+                return outputBuffer.ToArray();
+            }
+            catch (FormatException fe)
+            {
+                MessageBox.Show(String.Format("データ変換失敗：{0}", fe.Message));
+                return null;
+            }
+        }
+        #endregion
+    }
+}
